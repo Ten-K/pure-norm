702e5ad4176a4a56f6621921e5de600240f83650
lrl <742798240@qq.com>
2023-10-31 20:25:10 +0800

    feat: norm

--- index.ts
+++ index.ts
@@ -3,24 +3,25 @@
 import cac from "cac";
 import render from "./utils/render";
 import { setEnv } from "./utils/env";
+import { version } from "./package.json";
 import { debugError } from "./utils/debug";
 
 const cli = cac("pure-norm");
-cli.command("[root]").action(async (_root, options) => {
-	const cwd = process.cwd();
-	setEnv("cwd", cwd);
+cli.command("[root]").action(async _root => {
+  const cwd = process.cwd();
+  setEnv("cwd", cwd);
 
-	const init = async (cwd: string) => {
-		try {
-			render(cwd);
-		} catch (error) {
-			debugError(error);
-		}
-	};
+  const init = async (cwd: string) => {
+    try {
+      render(cwd);
+    } catch (error) {
+      debugError(error);
+    }
+  };
 
-	init(cwd);
+  init(cwd);
 });
 
 cli.help();
-cli.version("1.0.0");
+cli.version(version);
 cli.parse();
